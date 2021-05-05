c0117ef714bad710633e71e011b50fc915bf077f
colTaylor3 <80934161+colTaylor3@users.noreply.github.com>
2021-05-04 23:53:44 -0500

    Add files via upload

---
+++ Current Date and Time.js
@@ -0,0 +1,36 @@
+var today = new Date();
+var dayOfTheMonth = today.getDate();
+
+var mm = today.getMonth() + 1;
+var yy = today.getFullYear();
+
+
+today = mm+ '-' + dayOfTheMonth + '-' + yy;
+console.log(today);
+
+
+
+
+
+
+
+
+function interviewQuestion(question) {
+    var x = question
+
+
+    if(x === "Tell us about yourself") {
+        console.log("My Name is Colton Taylor, I am currently a software tester, but aspire to be a developer. Thanks for asking");
+
+        } if else (x = "What seperates you from other candidates") {
+            console.log("I am a very driven person, and I already have a very in depth knowledge of our products");
+
+            } else(x = "Any other questions for us?") {
+                console.log("I have no more questions for your guys, thanks for the time in interviewing me, have a good day");
+
+        }
+
+
+}
+
+
